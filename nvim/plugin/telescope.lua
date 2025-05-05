d57b8a6f98e2609b332881c133f36546cdea326c
NovaViper <coder.nova99@mailbox.org>
2025-05-05 14:06:37 -0500

    refactor(telescope): Load fzf extension and more sane defaults

--- nvim/plugin/telescope.lua
+++ nvim/plugin/telescope.lua
@@ -3,19 +3,32 @@ local builtin = require("telescope.builtin")
 
 telescope.setup({
   defaults = {
-    i = {
-      -- map actions.which_key to <C-h> (default: <C-/>)
-      -- actions.which_key shows the mappings for your picker,
-      -- e.g. git_{create, delete, ...}_branch for the git_branches picker
-      ["<C-h>"] = "which_key",
+    prompt_prefix = " ",
+    selection_caret = " ",
+    path_display = { "smart" },
+    mappings = {
+      i = {
+        -- map actions.which_key to <C-h> (default: <C-/>)
+        -- actions.which_key shows the mappings for your picker,
+        -- e.g. git_{create, delete, ...}_branch for the git_branches picker
+        ["<C-h>"] = "which_key",
+      },
+    },
+  },
+  --pickers = {},
+  extensions = {
+    fzf = {
+      fuzzy = true, -- false will only do exact matching
+      override_generic_sorter = true, -- override the generic sorter
+      override_file_sorter = true, -- override the file sorter
+      case_mode = "smart_case", -- or "ignore_case" or "respect_case"
+      -- the default case_mode is "smart_case"
     },
   },
 })
 
--- To get fzf loaded and working with telescope, you need to call
--- load_extension, somewhere after setup function:
-require("telescope").load_extension("fzf")
+telescope.load_extension("fzf")
 
 nnoremap("<leader>fg", builtin.live_grep, "Telescope live grep")
-nnoremap("<leader>fb", builtin.buffers, "Telescope buffers")
 nnoremap("<leader>fh", builtin.help_tags, "Telescope help tags")
+nnoremap("<space><space>", builtin.buffers, "Telescope find buffer")
