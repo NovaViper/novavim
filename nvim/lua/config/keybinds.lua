47a65880ab17da0f102d086a5e8560365c4412c5
NovaViper <coder.nova99@mailbox.org>
2025-05-06 09:25:18 -0500

    refactor: better tab and yazi keymaps and make yazi use snacks.picker

--- nvim/lua/config/keybinds.lua
+++ nvim/lua/config/keybinds.lua
@@ -96,3 +96,16 @@ nnoremap("<leader>fs", function()
   local filename = vim.fn.input("Enter Filename: ")
   vim.cmd("write " .. filename)
 end, "Save file")
+
+-- Tab management
+nnoremap("th", ":tabprev<CR>")
+nnoremap("tl", ":tabnext<CR>")
+
+nnoremap("tf", ":tabnew New Tab<CR>") -- Create fresh tab
+nnoremap("td", ":tabclose<CR>")
+nnoremap("t!", ":quit!<CR>")
+
+-- Go to specific tab number
+for i = 1, 9 do
+  nnoremap("t" .. i, i .. "gt")
+end
--- nvim/plugin/yazi.lua
+++ nvim/plugin/yazi.lua
@@ -2,7 +2,25 @@ local yazi = require("yazi")
 
 yazi.setup({
   --open_for_directories = true,
+  open_multiple_tabs = true,
+  integrations = {
+    grep_in_directory = "snacks.picker",
+    grep_in_selected_files = "snacks.picker",
+    picker_add_copy_relative_path_action = "snacks.picker",
+  },
 })
 
 -- Force yaz to replace netrw
 --vim.g.loaded_netrwPlugin = 1
+
+nnoremap("<leader>y", function()
+  yazi.yazi()
+end, "Open yazi at current file")
+
+noremap("<leader>yy", function()
+  local cfg = {}
+  local cwd = vim.fn.getcwd()
+  yazi.yazi(cfg, cwd)
+end, "Open yazi at working directory")
+
+noremap("<c-up>", "<cmd>Yazi toggle<cr>", "Resume the last yazi session")
