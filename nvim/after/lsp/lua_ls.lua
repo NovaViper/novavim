9b73e97af058fe8ed55385c4e1be69061054606d
NovaViper <coder.nova99@mailbox.org>
2025-05-10 17:48:39 -0500

    refactor(lua_lsp): Disable annoying warnings and fix typo

--- nvim/after/lsp/lua_ls.lua
+++ nvim/after/lsp/lua_ls.lua
@@ -1,14 +1,18 @@
 return {
   settings = {
-    lua = {
-      hint = {
-        enable = true,
-        arrayIndex = "Disable", -- this disables the little definitions in the arrays, which was really annoying to have
-      },
-
+    Lua = {
       diagnostics = {
         -- Neovim and Yazi globals
         globals = { "vim", "require", "ya", "cx", "Command" },
+        -- NOTE: toggle below to ignore Lua_LS's noisy `missing-fields` warnings
+        -- https://github.com/nvim-lua/kickstart.nvim/issues/543#issuecomment-1859319206
+        disable = { "missing-fields", "lowercase-global" },
+      },
+      hint = {
+        enable = true,
+
+        -- this disables the little definitions in the arrays, which was really annoying to have
+        arrayIndex = "Disable",
       },
     },
   },
