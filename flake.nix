cfbfa50fea9fa2955f66236ee859c76822f5d833
NovaViper <coder.nova99@mailbox.org>
2025-05-15 00:09:44 -0500

    treewide: restructure p1 Basic restructuring of some core files. Merged together flake.nix and outputs.nix into one file for readability. Also moved lazy and nonLazy into the root; planning on restructuring the file names next.

--- flake.nix
+++ flake.nix
@@ -1,6 +1,4 @@
 {
-  outputs = args: import ./outputs.nix args;
-
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     mnw.url = "github:Gerg-L/mnw";
@@ -11,4 +9,87 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
+
+  outputs =
+    {
+      self,
+      nixpkgs,
+      neovimPlugins,
+      systems,
+      mnw,
+      ...
+    }@inputs:
+    let
+      inherit (nixpkgs) lib;
+      sys = import systems;
+
+      forEachSystem = function: lib.genAttrs sys (system: function pkgsFor.${system});
+      pkgsFor = lib.genAttrs sys (
+        system:
+        import nixpkgs {
+          inherit system;
+          #overlays = builtins.attrValues self.outputs.overlays;
+          #config.allowUnfree = true;
+        }
+      );
+    in
+    {
+      packages = forEachSystem (
+        pkgs:
+        let
+          # Lists of derivations that we grab from external sources (nixpkgs and
+          # neovimPlugins)
+          # Check https://github.com/NixNeovim/NixNeovimPlugins/blob/main/plugins.md for updates
+          nonLazyPlugins = import ./nonLazy.nix { inherit pkgs neovimPlugins; };
+          lazyPlugins = import ./lazy.nix { inherit pkgs neovimPlugins; };
+          packages = import ./packages.nix { inherit pkgs; };
+        in
+        {
+          default = mnw.lib.wrap pkgs {
+            appName = "nvim";
+            neovim = pkgs.neovim-unwrapped;
+            initLua =
+              # lua
+              ''
+                -- Uncomment when you want to profile nvim startup. Be sure to have
+                -- the snacks.nvim repo cloned for this to work!
+
+                --vim.opt.rtp:append("~/Documents/Repos/snacks.nvim")
+                --require("snacks.profiler").startup()
+
+                require("config")
+                require("lz.n").load('lazy')
+                 
+                -- Add to this whenever you add a new server to the `lsp` folder!
+                -- Ridiculous that nvim can't load them for you as far as I can tell
+                vim.lsp.enable({"bashls", "fish_lsp", "jsonls", "just", "lua_ls", "marksman", "nixd", "pyright", "rust_analyzer", "yamlls"})
+              '';
+
+            # List of plugins to load automatically
+            plugins.start = nonLazyPlugins;
+            # List of plugins to not load automatically (for lazy loading plugins)
+            plugins.opt = lazyPlugins;
+
+            # Extra lua packages (non vim plugins) to put into neovim's PATH
+            extraLuaPackages = ps: [ ps.magick ];
+
+            # Extra packages to be put in neovim's PATH
+            extraBinPath = packages;
+
+            # Plugins which can be reloaded without rebuilding
+            plugins.dev.config = {
+              pure = ./nvim;
+              impure = "~/Documents/Projects/novavim/nvim";
+            };
+          };
+        }
+      );
+
+      # Hotload neovim configs
+      devShells = forEachSystem (pkgs: {
+        default = pkgs.mkShellNoCC {
+          packages = [ self.packages.${pkgs.system}.default.devMode ];
+        };
+      });
+    };
 }
