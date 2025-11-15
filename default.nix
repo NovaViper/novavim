2ce7ae214763a08dbd1adce08529151793fcea24
NovaViper <coder.nova99@mailbox.org>
2025-11-14 20:05:22 -0600

    mnw: load init.lua from file
    Trim down the package file and also make the lua formatter available to
    the code directly instead of through nested treesitter

--- default.nix
+++ default.nix
@@ -22,22 +22,7 @@ in
 mnw.lib.wrap pkgs {
   appName = "nvim";
   neovim = pkgs.neovim-unwrapped;
-  initLua =
-    # lua
-    ''
-      -- Uncomment when you want to profile nvim startup. Be sure to have
-      -- the snacks.nvim repo cloned for this to work!
-
-      --vim.opt.rtp:append("~/Documents/Repos/snacks.nvim")
-      --require("snacks.profiler").startup()
-
-      require("config")
-      require("lz.n").load('lazy')
-       
-      -- Add to this whenever you add a new server to the `lsp` folder!
-      -- Ridiculous that nvim can't load them for you as far as I can tell
-      vim.lsp.enable({"bashls", "fish_lsp", "jsonls", "just", "lua_ls", "marksman", "nixd", "pyright", "rust_analyzer", "yamlls"})
-    '';
+  luaFiles = [ ./init.lua ];
 
   # List of plugins to load automatically
   plugins.start = startPlugins ++ builtins.attrValues customStartPlugins;
---
+++ init.lua
@@ -0,0 +1,23 @@
+-- Uncomment when you want to profile nvim startup. Be sure to have
+-- the snacks.nvim repo cloned for this to work!
+
+--vim.opt.rtp:append("~/Documents/Repos/snacks.nvim")
+--require("snacks.profiler").startup()
+
+require("config")
+require("lz.n").load("lazy")
+
+-- Add to this whenever you add a new server to the `lsp` folder!
+-- Ridiculous that nvim can't load them for you as far as I can tell
+vim.lsp.enable({
+  "bashls",
+  "fish_lsp",
+  "jsonls",
+  "just",
+  "lua_ls",
+  "marksman",
+  "nixd",
+  "pyright",
+  "rust_analyzer",
+  "yamlls",
+})
