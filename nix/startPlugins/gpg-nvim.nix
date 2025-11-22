366cf1b07a1db45ee1b8a908f209937c39e0ff81
NovaViper <coder.nova99@mailbox.org>
2025-11-21 22:07:04 -0600

    packages: update gpg.nvim rev

--- nix/startPlugins/gpg-nvim.nix
+++ nix/startPlugins/gpg-nvim.nix
@@ -1,12 +1,12 @@
 { vimUtils, fetchFromGitHub, ... }:
 vimUtils.buildVimPlugin {
   name = "gpg-nvim";
-  version = "2025-05-30";
+  version = "2025-10-30";
 
   src = fetchFromGitHub {
     owner = "benoror";
     repo = "gpg.nvim";
-    rev = "2e35ea9a94eb41e9f14224490f4391b72c3780d7";
-    hash = "sha256-wWqzT8itSuIZgVBvQIXZmSn0MqvOg0i4MaKArg/2eS4=";
+    rev = "e86411b07863718169d634c6361f0e0120ed7a83";
+    hash = "sha256-leolCPQN4IVz2eAAsY4ZlBNjzv77ZJWmMDzv5o/BAOI=";
   };
 }
