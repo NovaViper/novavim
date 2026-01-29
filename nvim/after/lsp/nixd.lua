60d7df63dfc31a225b1ad7e5c3d29b8c7d22f6bf
NovaViper <coder.nova99@mailbox.org>
2026-01-28 23:22:45 -0600

    nixd: much better lsp options

--- nvim/after/lsp/nixd.lua
+++ nvim/after/lsp/nixd.lua
@@ -1,12 +1,27 @@
+local hostname = vim.fn.hostname()
+local username = vim.env.USER
 return {
+  settings = {
+    nixd = {
+      options = {
+        nixos = {
+          expr = string.format(
+            '(builtins.getFlake ("git+file://" + toString ./.)).nixosConfigurations.%s.options',
+            hostname
+          ),
+        },
+        home_manager = {
+          expr = string.format(
+            '(builtins.getFlake ("git+file://" + toString ./.)).nixosConfigurations.%s.options.home-manager.users.type.getSubOptions []',
+            hostname
+          ),
+        },
+      },
+    },
+  },
   cmd = {
     "nixd",
     "--inlay-hints=false",
     "--semantic-tokens=true", -- NEEDED, makes syntax highlighting much better
   },
-  -- settings.nixd =
-  -- {
-  --  nixpkgs.expr = "import <nixpkgs> { }",
-  --   options.nixos.expr = "${hostOptions}",
-  -- },
 }
