71add0393b8d5a5925873b94112e50d6a0f21481
NovaViper <coder.nova99@mailbox.org>
2026-01-31 22:04:20 -0600

    cmp: much saner settings

--- nvim/plugin/cmp.lua
+++ nvim/plugin/cmp.lua
@@ -14,21 +14,23 @@ cmp.setup({
     keyword = { range = "full" },
     list = {
       selection = {
+        -- Always select the first item
         preselect = true,
         -- ghosttext is preferable
         auto_insert = false,
       },
     },
-
     ghost_text = { enabled = true },
+
+    -- I want the documentation to show!
     documentation = {
       auto_show = true,
       auto_show_delay_ms = 0,
     },
+    -- Nice renders
     menu = {
       draw = {
         columns = { { "kind_icon" }, { "label", "label_description" } },
-
         components = {
           label = {
             -- Removes label_details from being added to label by default
@@ -39,7 +41,7 @@ cmp.setup({
       },
     },
   },
-  sources = { -- Removing buffer completion from the defaults
+  sources = {
     default = { "lsp", "path", "snippets", "buffer", "lazydev" },
     -- Loading lazydev through blink leads to better signature help and overall
     -- a better experience
@@ -52,19 +54,30 @@ cmp.setup({
       },
     },
   },
+  --Show lsp signatures for functions!
   signature = {
     enabled = true,
     window = { show_documentation = true },
   },
+  --Better cmdline completion defaults
   cmdline = {
     enabled = true,
     completion = {
       list = { selection = { preselect = true } },
       menu = { auto_show = true },
+      ghost_text = { enabled = true },
+    },
+    -- Much better keybindings for the cmdline
+    keymap = {
+      ["<Tab>"] = { "accept" },
+      ["<CR>"] = { "accept_and_enter", "fallback" },
+      ["<Down>"] = { "select_next", "fallback" },
+      ["<Up>"] = { "select_prev", "fallback" },
     },
   },
   -- Prioritizes exact matches higher
   fuzzy = {
+    --Ensure we're using the rust version of the fuzzy finder
     implementation = "prefer_rust_with_warning",
     sorts = {
       "exact",
