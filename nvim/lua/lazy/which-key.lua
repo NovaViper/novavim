ab730ced37c32646a0969f9d99677b020191b968
NovaViper <coder.nova99@mailbox.org>
2026-02-09 12:54:00 -0600

    which-key: remove uncessary keybind hiding

--- nvim/lua/lazy/which-key.lua
+++ nvim/lua/lazy/which-key.lua
@@ -13,7 +13,6 @@ return {
       {
         { "<leader>f", group = "file" }, -- group
         { "<leader>fn", desc = "Create New File/Buffer" },
-        { "<leader>f1", hidden = true }, -- hide this keymap
         { "<leader>w", proxy = "<c-w>", group = "windows" }, -- proxy to window mappings
       },
     })
