047e5d64517adf8b5f27cbb0b07ab468b9bcb7e1
NovaViper <coder.nova99@mailbox.org>
2026-02-09 22:48:40 -0600

    copilot: add several enchantments to codecompanion

--- nvim/plugin/copilot.lua
+++ nvim/plugin/copilot.lua
@@ -16,18 +16,39 @@ copilot.setup({
 })
 
 require("codecompanion").setup({
-  display = { action_palette = { provider = "snacks" } },
+  -- See https://github.com/olimorris/codecompanion.nvim/blob/main/lua/codecompanion/config.lua#L486 for keymaps
+  interactions = {
+    chat = {
+      -- Force all interactions to use snacks.nvim
+      slash_commands = {
+        ["buffer"] = { opts = { provider = "snacks" } },
+        ["fetch"] = { opts = { provider = "snacks" } },
+        ["file"] = { opts = { provider = "snacks" } },
+        ["help"] = { opts = { provider = "snacks" } },
+        ["image"] = { opts = { provider = "snacks" } },
+        ["symbols"] = { opts = { provider = "snacks" } },
+      },
+      opts = {
+        -- Ensure we're using blink.cmp
+        completion_provider = "blink",
+      },
+    },
+  },
+  display = {
+    action_palette = { provider = "snacks" },
+    chat = {
+      auto_scoll = false,
+      fold_context = true,
+      show_settings = true, -- Show LLM settings at the top of the chat buffer
+      start_in_insert_mode = true, -- Start chat buffer in insert mode
+    },
+  },
   extensions = {
     spinner = {},
     history = {
       enabled = true,
       opts = { dir_to_save = vim.fn.stdpath("data") .. "/codecompanion_chats.json" },
     },
-  },
-  filetypes = {
-    markdown = true,
-    help = true,
-    python = true,
   },
 })
 
