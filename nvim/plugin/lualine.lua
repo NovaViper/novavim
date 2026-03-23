f83c5265fef93f4c9f61b054c894a0e9727d2808
NovaViper <coder.nova99@mailbox.org>
2026-03-23 08:53:25 -0500

    fix!: rename catppuccin theme since it changed upstream
    It was renamed to catppuccin-nvim, making lualine break

--- nvim/plugin/lualine.lua
+++ nvim/plugin/lualine.lua
@@ -2,7 +2,7 @@ local lualine = require("lualine")
 
 lualine.setup({
   options = {
-    theme = "catppuccin",
+    theme = "catppuccin-nvim",
     icons_enabled = true,
     component_separators = { left = "", right = "" },
     section_separators = { left = "", right = "" },
