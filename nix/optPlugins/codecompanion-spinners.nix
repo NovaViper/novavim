7ce2bb7c60f0c8eb5c37d33e527185863e88949c
NovaViper <coder.nova99@mailbox.org>
2026-07-15 07:45:51 -0500

    chore: bump packages

--- nix/optPlugins/codecompanion-spinners.nix
+++ nix/optPlugins/codecompanion-spinners.nix
@@ -1,12 +1,12 @@
 { fetchFromGitHub }:
 {
   name = "codecompanion-spinners-nvim";
-  version = "2025-10-11";
+  version = "2026-06-22";
 
   src = fetchFromGitHub {
     owner = "lalitmee";
     repo = "codecompanion-spinners.nvim";
-    rev = "86926cbf7554d69d40d2a5c3cf576063814a42d5";
-    hash = "sha256-L+vG4wj2O1VaiHhhjBAi26nglW0WnPSTk8FihkK8cn0=";
+    rev = "f613a543ca8c7ba1686830c6bb1a10d15a2e1942";
+    hash = "sha256-SOjECXjHu+JjGIAFDFroOyHsBqYGK3+xZ8f7w7yjFvQ=";
   };
 }
--- nix/startPlugins/codedocs-nvim.nix
+++ nix/startPlugins/codedocs-nvim.nix
@@ -1,12 +1,12 @@
 { fetchFromGitHub }:
 {
   name = "codedocs-nvim";
-  version = "2026-07-03";
+  version = "2026-07-13";
 
   src = fetchFromGitHub {
     owner = "jeangiraldoo";
     repo = "codedocs.nvim";
-    rev = "d20f3c4a72a7de3893593e33a157df2be5a9f60b";
-    hash = "sha256-0NKm28KhM14WiAcBmCa7cdVW4zILeR3AKgTyeAH69TY=";
+    rev = "880d125f48d6da5bdf5fc47ca56cc4a0fc66dac0";
+    hash = "sha256-aoYhVUSmjri2R3qH+9Js9YffSs8aLxlOL9NCCEzZICQ=";
   };
 }
